a40cc57c57b3cb77e5b08a3decc6b66e8b7f505b
Harsh Joshi <harshjoshi0106@gmail.com>
2026-06-11 10:32:16 +0530

    fix backend cors issues

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", process.env.FRONTEND_URL],
+    origin: ["http://localhost:3000", "http://localhost:3001", process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
     credentials: true,
   })
 );
